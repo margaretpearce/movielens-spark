f7bef77c3456155377c156dfe608ccac8531396d
Margaret Pearce <margaret.pearce@gmail.com>
2017-04-17 20:54:13 -0400

    Top rated

--- ratingsanalysis.py
+++ ratingsanalysis.py
@@ -11,7 +11,7 @@ movies = sqlContext.read\
           .option("header", "true")\
           .load("../MovieLensData/movies.csv")
 print("---Movies---")
-movies.show(3)
+movies.show(3, False)
 
 # Read in the ratings data set
 ratings = sqlContext.read\
@@ -19,16 +19,29 @@ ratings = sqlContext.read\
           .option("header", "true")\
           .load("../MovieLensData/ratings.csv")
 print("---Ratings---")
-ratings.show(3)
+ratings.show(3, False)
 
 # Compute the average rating by movie
 avg_rating = ratings.groupBy("movieId").agg({"rating": "avg", "userId": "count"})\
     .withColumnRenamed("avg(rating)", "avgRating")\
     .withColumnRenamed("count(userId)", "numRatings")
 print("---Average Rating---")
-avg_rating.show(5)
+avg_rating.show(5, False)
 
 # Join the ratings with the name of the movie
 ratings_with_titles = avg_rating.join(movies, "movieId")
 print("---Average Rating (with Title)---")
-ratings_with_titles.show(5)
+ratings_with_titles.show(5, False)
+
+# Show top 5 rated movies
+top_rated = ratings_with_titles.orderBy("avgRating", ascending=False).limit(5)
+print("--Top 5 rated movies--")
+top_rated.show(5, False)
+
+# Many of the top rated movies only have one rating
+# What if we filter for movies with more than 20 reviews?
+top_rated_filtered = ratings_with_titles.filter(ratings_with_titles["numRatings"] > 20)\
+                                        .orderBy("avgRating", ascending=False)\
+                                        .limit(5)
+print("--Top 5 rated movies (>20 reviews)--")
+top_rated_filtered.show(5, False)
