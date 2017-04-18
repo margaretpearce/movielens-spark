dfdd819dceccac0ebf350967930cc11d7de29734
Margaret Pearce <margaret.pearce@gmail.com>
2017-04-17 20:43:51 -0400

    Joining with title

--- ratingsanalysis.py
+++ ratingsanalysis.py
@@ -11,7 +11,7 @@ movies = sqlContext.read\
           .option("header", "true")\
           .load("../MovieLensData/movies.csv")
 print("---Movies---")
-print(movies.head())
+movies.show(3)
 
 # Read in the ratings data set
 ratings = sqlContext.read\
@@ -19,9 +19,16 @@ ratings = sqlContext.read\
           .option("header", "true")\
           .load("../MovieLensData/ratings.csv")
 print("---Ratings---")
-print(ratings.head())
+ratings.show(3)
 
 # Compute the average rating by movie
-avg_rating = ratings.groupBy("movieId").agg({"rating": "avg", "userId": "count"})
+avg_rating = ratings.groupBy("movieId").agg({"rating": "avg", "userId": "count"})\
+    .withColumnRenamed("avg(rating)", "avgRating")\
+    .withColumnRenamed("count(userId)", "numRatings")
 print("---Average Rating---")
-print(avg_rating.show(5))
+avg_rating.show(5)
+
+# Join the ratings with the name of the movie
+ratings_with_titles = avg_rating.join(movies, "movieId")
+print("---Average Rating (with Title)---")
+ratings_with_titles.show(5)
